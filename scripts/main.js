9924008552c818d5305480e2b564028fc29a591d
unathi Matiwane <110603176+Matiwane123@users.noreply.github.com>
2026-04-12 15:36:10 +0200

    CHECKING PROJECT ON NETLIFY

--- scripts/main.js
+++ scripts/main.js
@@ -9,49 +9,6 @@ const createTaskForm = document.getElementById("new-task-modal-window");
 // Local Storage Key
 const STORAGE_KEY = "kanbanTasks";
 
-// Initial tasks (seed data)
-const initialTasks = [
-  {
-    id: 1,
-    title: "Launch Epic Career 🚀",
-    description: "Create a killer Resume",
-    status: "todo",
-  },
-  {
-    id: 2,
-    title: "Master JavaScript 💛",
-    description: "Get comfortable with the fundamentals",
-    status: "doing",
-  },
-  {
-    id: 3,
-    title: "Keep on Going 🏆",
-    description: "You're almost there",
-    status: "doing",
-  },
-  {
-    id: 4,
-    title: "Learn Data Structures and Algorithms 📚",
-    description:
-      "Study fundamental data structures and algorithms to solve coding problems efficiently",
-    status: "todo",
-  },
-  {
-    id: 5,
-    title: "Contribute to Open Source Projects 🌐",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
-    status: "done",
-  },
-  {
-    id: 6,
-    title: "Build Portfolio Projects 🛠️",
-    description:
-      "Create a portfolio showcasing your skills and projects to potential employers",
-    status: "done",
-  },
-];
-
 // Load tasks from localStorage on startup
 document.addEventListener("DOMContentLoaded", () => {
   if (!localStorage.getItem(STORAGE_KEY)) {
